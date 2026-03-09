c8b724ac72802c4a3968735eed72ba9bd669e302
4rthurzinhoo <arthurlaoliveira@gmail.com>
2026-03-09 16:26:08 -0300

    Garantindo que a variavel livros seja um array

--- frontend/react-biblioteca/src/pages/Biblioteca/index.jsx
+++ frontend/react-biblioteca/src/pages/Biblioteca/index.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState, useRef } from "react"
 import { Link, useNavigate } from "react-router-dom"
 
 function Biblioteca() {
-    const [l, setLivros] = useState()
+    const [l, setLivros] = useState([])
 
     const [filtroNumero, setFiltroNumero] = useState("")
     const [filtroNome, setFiltroNome] = useState("")
